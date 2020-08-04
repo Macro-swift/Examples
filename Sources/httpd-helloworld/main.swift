a5408a80a7af6146a8cf5b994c93ba2ed07f4499
Helge Heß <devteam@zeezide.de>
2020-08-04 18:51:43 +0200

    Make HTTP server listen on wildcard IPv4
    ... easier to play with.

--- Sources/httpd-helloworld/main.swift
+++ Sources/httpd-helloworld/main.swift
@@ -21,6 +21,6 @@ http.createServer { req, res in
     // finish up
     res.end()
 }
-.listen(1337) { server in
-    console.log("Server listening on http://localhost:1337/")
+.listen(1337, "0.0.0.0") { server in
+    console.log("Server listening on http://0.0.0.0:1337/")
 }
