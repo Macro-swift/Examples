4e88f3c753859c43d415ca23c96d9cfffab36033
Helge Heß <devteam@zeezide.de>
2020-08-04 18:37:28 +0200

    Bump Macro versions to 0.5.4
    ... get X compilation working.

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
   
   dependencies: [
     .package(url: "https://github.com/Macro-swift/Macro.git",
-             from: "0.0.16"),
+             from: "0.5.4"),
     .package(url: "https://github.com/Macro-swift/MacroExpress.git",
-             from: "0.0.3"),
+             from: "0.5.4"),
     .package(url: "https://github.com/AlwaysRightInstitute/cows",
              from: "1.0.0")
   ],
