af15156df45f66f6e6b259e81736036b037abe9d
Helge Heß <devteam@zeezide.de>
2022-08-23 15:16:08 +0200

    Use Swift 5.4+
    Match what Macro now requires (due to NIO).

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.4
 
 import PackageDescription
 
@@ -14,7 +14,7 @@ let package = Package(
     .executable(name: "httpd-helloworld", targets: [ "httpd-helloworld" ]),
     .executable(name: "connect-static",   targets: [ "connect-static"   ]),
     .executable(name: "express-simple",   targets: [ "express-simple"   ]),
-    .executable(name: "httpproxy",        targets: [ "httpproxy"        ]),
+    //.executable(name: "httpproxy",        targets: [ "httpproxy"        ]),
     .executable(name: "servedocc",        targets: [ "servedocc"        ]),
     .executable(name: "todomvc",          targets: [ "todomvc"          ]),
     .executable(name    :   "express-simple-lambda",
@@ -25,24 +25,38 @@ let package = Package(
     // A lot of packages for demonstration purposes, only add what you
     // actually need in your own project.
     .package(url: "https://github.com/Macro-swift/Macro.git",
-             from: "0.8.11"),
+             from: "0.9.0"),
     .package(url: "https://github.com/Macro-swift/MacroExpress.git",
-             from: "0.8.8"),
+             from: "0.9.0"),
     .package(url: "https://github.com/Macro-swift/MacroLambda.git",
              from: "0.4.1"),
     .package(url: "https://github.com/AlwaysRightInstitute/cows",
-             from: "1.0.7")
+             from: "1.0.10")
   ],
   
   targets: [
-    .target(name: "httpd-helloworld", dependencies: [ "Macro"        ]),
-    .target(name: "httpproxy",        dependencies: [ "Macro"        ]),
-    .target(name: "connect-static",   dependencies: [ "MacroExpress" ]),
-    .target(name: "express-simple",   dependencies: [ "MacroExpress", "cows" ]),
-    .target(name: "servedocc",        dependencies: [ "MacroExpress" ]),
-    .target(name: "todomvc",          dependencies: [ "MacroExpress" ]),
+    .executableTarget(name: "httpd-helloworld",
+                      dependencies: [ "Macro" ],
+                      exclude: [ "README.md" ]),
+    //  .target(name: "httpproxy", dependencies: [ "Macro" ]),
+    .executableTarget(name: "connect-static",
+                      dependencies: [ "MacroExpress" ],
+                      exclude: [ "README.md" ],
+                      resources: [ .copy("public") ]),
+    .executableTarget(name: "express-simple",
+                      dependencies: [ "MacroExpress", "cows" ],
+                      resources: [ .copy("public"), .copy("views") ]),
+    .executableTarget(name: "servedocc",
+                      dependencies: [ "MacroExpress" ],
+                      exclude: [ "README.md" ]),
+    .executableTarget(name: "todomvc",
+                      dependencies: [ "MacroExpress" ],
+                      exclude: [ "README.md" ],
+                      resources: [ .copy("public") ]),
 
-    .target(name: "express-simple-lambda",
-            dependencies: [ "MacroLambda", "cows" ])
+    .executableTarget(name: "express-simple-lambda",
+                      dependencies: [ "MacroLambda", "cows" ],
+                      exclude: [ "README.md" ],
+                      resources: [ .copy("public"), .copy("views") ])
   ]
 )
